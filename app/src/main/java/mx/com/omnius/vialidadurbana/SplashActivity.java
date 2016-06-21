3212e6223a499af426f08cd03f19c7aa429bfa36
Ramon Gomez Romero <ultra_rgr@hotmail.com>
2016-06-20 20:16:07 -0500

    Se agrega Splash Activity y recursos

--- app/src/main/java/mx/com/omnius/vialidadurbana/SplashActivity.java
+++ app/src/main/java/mx/com/omnius/vialidadurbana/SplashActivity.java
@@ -1,13 +1,34 @@
 package mx.com.omnius.vialidadurbana;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ImageView splash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        splash = (ImageView) findViewById(R.id.imgLogo);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.animacion);
+        splash.startAnimation(animation);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                direccionaPantalla();
+            }
+        }, 3200);
+    }
+
+    public void direccionaPantalla(){
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
